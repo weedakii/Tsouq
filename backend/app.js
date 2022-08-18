54e020d9dc7f9ca2038c75c103e98341c657c24c
weedakii <weeds0250@gmail.com>
2022-08-18 13:53:02 +0200

    changes

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,9 @@ import router from './routes/router.js';
 import path from 'path'
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+    origin: ['https://tsouq.herokuapp.com', 'https://tsouq-store.com']
+}))
 
 if (process.env.NODE_ENV !== "PRODUCTION") {
     dotenv.config({path: 'backend/config/config.env'})
